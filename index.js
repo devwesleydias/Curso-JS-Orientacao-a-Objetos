044d64ef4655a6b42d37688ce19b89dc8b93b544
devwesleydias <git@github.com:devwesleydias>
2021-12-17 16:33:41 -0300

    interfaces e heranca

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
 
-let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaSalario);
